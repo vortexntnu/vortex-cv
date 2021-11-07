96b4e768bf07abcf2fcfc7740c3ece99470c8fba
Erik Rowe <ekrowe@stud.ntnu.no>
2021-11-07 20:34:21 +0100

    two lense detection creates single point in the middle

--- object_detection/scripts/object_detection_node.py
+++ object_detection/scripts/object_detection_node.py
@@ -39,6 +39,9 @@ class ObjectDetectionNode():
         ArrayBoundingBoxes.header = data.header
         ArrayBoundingBoxes.image_header = data.image_header
 
+        # Dictionary for saving object to see if the same object is detected twice
+        saved_objects = {}
+
         # Iterate through all the detected objects and estimate sizes
         for bbox in data.bounding_boxes:
 
@@ -62,6 +65,22 @@ class ObjectDetectionNode():
             CurrentBoundingBox.centre_angle_x = redefined_angle_x
             CurrentBoundingBox.centre_angle_y = redefined_angle_y
 
+            # Get the position of the object relative to the camera
+            vector = self.coord_positioner.main(redefined_angle_x, redefined_angle_y, depth_mtr)
+
+            # If the same object is detected in each lense then a point in the middle is calculated
+            key_string = str(bbox.Class)
+            if (key_string in saved_objects):
+                value = saved_objects.get(key_string)
+                new_x = (value[0]+vector[0])*0.5
+                new_y = (value[1]+vector[1])*0.5
+                new_z = (value[2]+vector[2])*0.5
+                saved_objects[str(bbox.Class)] = [new_x, new_y, new_z]
+            else:
+                saved_objects[str(bbox.Class)] = [vector[0], vector[1], vector[2]]
+            
+            # rospy.loginfo(saved_objects)
+
             # Append the new message to bounding boxes array
             ArrayBoundingBoxes.bounding_boxes.append(CurrentBoundingBox)
             
@@ -72,12 +91,10 @@ class ObjectDetectionNode():
             new_point = PointStamped()
             new_point.header = data.header
             new_point.header.stamp = rospy.get_rostime()
-
-            # Get the position of the object relative to the camera
-            vector = self.coord_positioner.main(redefined_angle_x, redefined_angle_y, depth_mtr)
-            new_point.point.x = vector[0]
-            new_point.point.y = vector[1]
-            new_point.point.z = vector[2]
+            corrected_point = saved_objects.get(key_string)
+            new_point.point.x = corrected_point[0]
+            new_point.point.y = corrected_point[1]
+            new_point.point.z = corrected_point[2]
 
             # Publish message for each detected object --> rethink this, is it smart to have this in the loop, or should a list of point be sendt instead.
             pointPub.publish(new_point)
