b68753e41ddc1130a2b2d9559a4a66d9c7352262
Stormtrooper <stormtrooper@vortexntnu.no>
2022-01-12 16:57:40 +0100

    clean_up

--- object_detection/scripts/object_detection_node.py
+++ object_detection/scripts/object_detection_node.py
@@ -6,9 +6,7 @@ import rospy
 from darknet_ros_msgs.msg import BoundingBoxes
 from cv_msgs.msg import BBox, BBoxes
 from geometry_msgs.msg import PointStamped
-from rospy.core import rospyinfo
 from vortex_msgs.msg import ObjectPosition
-from vortex_msgs.msg import PointStampedArray
 from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs import point_cloud2
 
@@ -19,11 +17,13 @@ from coord_pos import CoordPosition
 class ObjectDetectionNode():
     """Handles tasks related to object detection
     """
-    pc_data = 0
+    pointcloud_x = 0.0
+    pointcloud_y = 0.0
+    pointcloud_z = 0.0
 
     def __init__(self):
         rospy.init_node('object_detection_node')
-        self.bboxSub = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.bboxSub_cb_single_lense)
+        self.bboxSub = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.bboxSub_cb)
         self.pcSub = rospy.Subscriber('/zed2/zed_node/point_cloud/cloud_registered', PointCloud2, self.pointcloud_cb)
         self.pcPub = rospy.Publisher('/object_detection/bbox_pointcloud', PointCloud2, queue_size= 1)
         self.estimatorPub = rospy.Publisher('/object_detection/size_estimates', BBoxes, queue_size= 1)
@@ -31,40 +31,54 @@ class ObjectDetectionNode():
         self.position_estimator = PositionEstimator()
         self.coord_positioner = CoordPosition()
 
-    def pointcloud_cb(self, data):
-        self.pc_data = data
-        return
-
-    def send_pointcloud(self, bbox_data):
-        """"""
-        x_from = bbox_data.xmin
-        x_to = bbox_data.xmax
-        y_from = 376 - bbox_data.ymax
-        y_to = 376 - bbox_data.ymin
-
-
-        x = 0.0
-        y = 0.0
-        z = 0.0
-
-        # Generates a readable version of the point cloud data
-        assert isinstance(self.pc_data, PointCloud2)
-        is_pointcloud = isinstance(self.pc_data, PointCloud2)
-        if is_pointcloud:
-            # Reads the point cloud data at given u = x
-            pt_gen = point_cloud2.read_points(self.pc_data, skip_nans=False, uvs=[[x_from, y_from]])
-            for pt in pt_gen:
-                x = pt[0]
-                y = pt[1]
-                z = pt[2]
-                rospy.loginfo('%f %f %f',x ,y, z)
-
-        self.rviz_point_single_lense(self.test_data.header, [x,y,z], "pc_test")
-
-
-    def bboxSub_cb_single_lense(self, data):
+    def pointcloud_cb(self, msg_data):
         """
-        
+        Stores message from a PointCloud2 message into a class variable
+
+        Args:
+            msg_data: callback message from subscription
+
+        Returns:
+            class variable: self.pointcloud_data
+        """
+        self.pointcloud_data = msg_data
+
+    def send_pointcloud(self, x_pixel, y_pixel):
+        """
+        Reads the point cloud data from a given x, y coordinate
+
+        Args:
+            x_pixel: position in x direction of point you want clouddata from
+            y_pixel: position in y direction of point you want clouddata from
+
+        Returns:
+            Point cloud data for a point in the camera frame as list [x, y, z]
+        """
+        # Generates a readable version of the point cloud data
+        assert isinstance(self.pointcloud_data, PointCloud2)
+        is_pointcloud = isinstance(self.pointcloud_data, PointCloud2)
+        if is_pointcloud:
+            # Reads the point cloud data at given uvs: u = x cord, v = y cord
+            pt_gen = point_cloud2.read_points(self.pointcloud_data, skip_nans=False, uvs=[[x_pixel, y_pixel]])
+            for pt in pt_gen:
+                self.pointcloud_x = pt[0]
+                self.pointcloud_y = pt[1]
+                self.pointcloud_z = pt[2]
+
+        x, y, z = self.pointcloud_x, self.pointcloud_y, self.pointcloud_z
+        return [x, y, z]
+
+
+    def bboxSub_cb(self, data):
+        """
+        Gets the data from the subscribed message BoundingBoxes and publishes the size estimates of a detected object, and the position of the object.
+
+        Args:
+            data: The message that has been recieved.
+
+        Returns:
+            Published topics:
+                estimatorPub: Array of detected objects as the estimated size of these. Topic also includes angles to the objects from the camera frame.
         """
         self.test_data = data
         # Allocates msg data to local variables in order to process abs size
@@ -103,16 +117,25 @@ class ObjectDetectionNode():
 
             # Create rviz point
             key_string = str(bbox.Class)
-            self.rviz_point_single_lense(data.header, position, key_string)
+            self.rviz_point(data.header, position, key_string)
 
             
         self.estimatorPub.publish(ArrayBoundingBoxes)
 
-    def rviz_point_single_lense(self, headerdata, position, name):
+    def rviz_point(self, headerdata, position, name):
+        """
+        Publishes a PointStamped as a topic under /object_detection/object_point
+
+        Args:
+            headerdata: Headerdata to be used as a header will not be created in this function
+            position: A position xyz in the form [x, y, z] where xyz are floats
+            name: name to be given to the point published, must not contain special characters.
+
+        Returns:
+            Topic:
+                /object_detection/object_point/name where name is your input
+        """
         # For testing
-        for i in position:
-            if i != float:
-                i = 0.0
         pointPub = rospy.Publisher('/object_detection/object_point/' + name, PointStamped, queue_size= 1)
         new_point = PointStamped()
         new_point.header = headerdata
@@ -121,7 +144,6 @@ class ObjectDetectionNode():
         new_point.point.y = position[1]
         new_point.point.z = position[2]
         pointPub.publish(new_point)
-        # rospy.loginfo(new_point)
 
     def bboxSub_cb_twin_lense(self, data):
         """
@@ -181,34 +203,15 @@ class ObjectDetectionNode():
             else:
                 saved_objects[str(bbox.Class)] = [position[0], position[1], position[2]]
 
-            self.rviz_point(data, saved_objects, key_string)
+            self.rviz_point_old(data, saved_objects, key_string)
 
             # Append the new message to bounding boxes array
             ArrayBoundingBoxes.bounding_boxes.append(CurrentBoundingBox)
 
         self.estimatorPub.publish(ArrayBoundingBoxes)
 
-    def rviz_point(self, data, dictionary, key_string):
+    def rviz_point_old(self, data, dictionary, key_string):
         """Delete this when no longer needed"""
-        # Create a publisher for each detected object. The name of the topic will be the name of the object from class in bounding box message.
-        # # Build the message to place detected obejct in relation to camera 
-
-        # pointPub = rospy.Publisher('/object_detection/object_points', PointStampedArray, queue_size= 1)
-        # points = PointStampedArray()
-        # points.header = data.header
-        # points.image_header = data.image_header
-
-        # for i in dictionary:
-        #     corrected_point = dictionary.get(key_string)
-        #     new_point = PointStamped()
-        #     new_point.header = data.header
-        #     new_point.header.stamp = rospy.get_rostime()
-        #     new_point.point.x = corrected_point[0]
-        #     new_point.point.y = corrected_point[1]
-        #     new_point.point.z = corrected_point[2]
-        #     points.positions.append(new_point)
-
-        # pointPub.publish(points)
 
         pointPub = rospy.Publisher('/object_detection/object_point/' + key_string, PointStamped, queue_size= 1)
         new_point = PointStamped()
