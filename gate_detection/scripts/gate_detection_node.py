9c677a21c79fdbcc16e649662f99c924d9d1e143
Stormtrooper <stormtrooper@vortexntnu.no>
2022-01-13 00:30:56 +0100

    Add corner detection.

--- gate_detection/scripts/gate_detection_node.py
+++ gate_detection/scripts/gate_detection_node.py
@@ -31,7 +31,7 @@ class GateDetectionNode():
         self.hsvCheckPub = rospy.Publisher('/gate_detection/hsv_check_image', Image, queue_size= 1)
         self.noiseRmPub = rospy.Publisher('/gate_detection/noise_removal_image', Image, queue_size= 1)
         self.contourPub = rospy.Publisher('/gate_detection/contour_image', Image, queue_size= 1)
-        self.fillsPub = rospy.Publisher('/gate_detection/fills_image', Image, queue_size= 1)
+        self.cornersPub = rospy.Publisher('/gate_detection/corners_image', Image, queue_size= 1)
 
         self.timerPub = rospy.Publisher('/gate_detection/timer', Float32, queue_size= 1)
 
@@ -69,9 +69,6 @@ class GateDetectionNode():
     
 
     def lines_publisher(self, orig_img, edges_img):
-        start = timer() # Start function timer.
-        #------------------------------------------>
-
         orig_img_cp = copy.deepcopy(orig_img)
 
         lines = cv2.HoughLines(edges_img, 1, np.pi/180, 200)
@@ -92,17 +89,6 @@ class GateDetectionNode():
                 lines_matrix.append([(x1,y1),(x2,y2)])
                 cv2.line(orig_img_cp, (x1,y1), (x2,y2), (0,0,255), 2)
         # print(lines_matrix)
-
-        ros_image = self.bridge.cv2_to_imgmsg(orig_img_cp, encoding="bgra8")
-        self.linesPub.publish(ros_image)
-
-        #------------------------------------------>
-        end = timer() # Stop function timer.
-        timediff = (end - start)
-
-        fps = 1 / timediff # Take reciprocal of the timediff to get runs per second. 
-
-        self.timerPub.publish(fps)
 
 
     def hsv_publisher(self, orig_img):
@@ -127,10 +113,12 @@ class GateDetectionNode():
         return hsv_mask
         # rospy.loginfo("\nHue: %d %d\nSat: %d %d\nVal: %d %d\n", self.hsv_hue_min, self.hsv_hue_max, self.hsv_sat_min, self.hsv_sat_max, self.hsv_val_min, self.hsv_val_max)
 
+
     def noise_removal(self, hsv_mask):
         hsv_mask_cp = copy.deepcopy(hsv_mask)
 
-        blur_hsv_img = cv2.GaussianBlur(hsv_mask_cp, (self.ksize1, self.ksize2), self.sigma)
+        blur_hsv_img = cv2.GaussianBlur(hsv_mask_cp, (9, 17), 10)
+        # blur_hsv_img = cv2.GaussianBlur(hsv_mask_cp, (self.ksize1, self.ksize2), self.sigma)
         
         thr_img = cv2.adaptiveThreshold(blur_hsv_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, self.thresholding_blocksize, self.thresholding_C)
 
@@ -144,56 +132,64 @@ class GateDetectionNode():
         return dilation_img
 
 
+    def contour_filtering(self, hierarchy, contours):
+        cnt_filter = []
+
+        for cnt_idx in range(len(contours)):
+            cnt_hier = hierarchy[0][cnt_idx]
+
+            # if len([i for i, j in zip(hierarchy[0][cnt_idx], [-1, -1, -1, cnt_idx - 1]) if i == j]) != 4:
+
+            if ((cnt_hier[0] == cnt_idx + 1) or (cnt_hier[0] == -1)) and ((cnt_hier[1] == cnt_idx - 1) or (cnt_hier[1] == -1)) and (cnt_hier[2] == -1):
+                cnt = contours[cnt_idx]
+                cnt_area = cv2.contourArea(cnt)
+                if cnt_area < 300:
+                    cnt_filter.append(False)
+                else:
+                    cnt_filter.append(True)
+            else:
+                cnt_filter.append(False)
+        return cnt_filter
+
+
     def contour_processing(self, orig_img, noise_removed_img, enable_convex_hull=False):
         orig_img_cp = copy.deepcopy(orig_img)
 
         # contours, hierarchy = cv2.findContours(noise_removed_img, 1, 2)
         # contours, hierarchy = cv2.findContours(noise_removed_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours, hierarchy = cv2.findContours(noise_removed_img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-        contours_filter = []
-        centroid_data = []
-        
-        for cnt_idx in range(len(contours)):
-            if len([i for i, j in zip(hierarchy[0][cnt_idx], [-1, -1, -1, cnt_idx - 1]) if i == j]) != 4:
-                contours_filter.append(False)
-            else:
-                cnt = contours[cnt_idx]
-                cnt_moments = cv2.moments(cnt)
 
-                centroid_center_x = int(cnt_moments['m10']/cnt_moments['m00'])
-                centroid_center_y = int(cnt_moments['m01']/cnt_moments['m00'])
-
-                cnt_area = cnt_moments['m00']
-
-                if cnt_area < 300:
-                    contours_filter.append(False)
-                else:
-                    contours_filter.append(True)
-                    centroid_data.append((centroid_center_x, centroid_center_y, cnt_area))
-
+        cnt_fiter = self.contour_filtering(hierarchy, contours)
         contours_array = np.array(contours)
-        contours_filtered = contours_array[contours_filter]
+        contours_filtered = contours_array[cnt_fiter]
 
+        using_contours = []
         if enable_convex_hull:
             hull_array = []
             for cnt_idx in range(len(contours_filtered)):
                 hull_array.append(cv2.convexHull(contours_filtered[cnt_idx], False))
+            using_contours = hull_array
+        else:
+            using_contours = contours_filtered
 
+
+        centroid_data = []
         for cnt_idx in range(len(contours_filtered)):
-            cnt_area_str = str(centroid_data[cnt_idx][2])
 
-            if enable_convex_hull:
-                cv2.drawContours(orig_img_cp, hull_array, cnt_idx, (255,0,0), 2)
-            else:
-                cv2.drawContours(orig_img_cp, contours_filtered, cnt_idx, (255,0,0), 2)
+            cnt = using_contours[cnt_idx]
+            cnt_moments = cv2.moments(cnt)
+
+            centroid_center_x = int(cnt_moments['m10']/cnt_moments['m00'])
+            centroid_center_y = int(cnt_moments['m01']/cnt_moments['m00'])
+
+            cnt_area = cnt_moments['m00']
+            cv2.drawContours(orig_img_cp, using_contours, cnt_idx, (255,0,0), 2)
+
+            centroid_data.append((centroid_center_x, centroid_center_y, cnt_area))
+            cnt_area_str = str(centroid_data[cnt_idx][2])
 
             orig_img_cp = cv2.circle(orig_img_cp, (centroid_data[cnt_idx][0], centroid_data[cnt_idx][1]), 2, (0,255,0), 2)
             orig_img_cp = cv2.putText(orig_img_cp, cnt_area_str, (centroid_data[cnt_idx][0], centroid_data[cnt_idx][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (1,0,0), 2)
-
-            # rect = cv2.minAreaRect(cnt)
-            # box = cv2.boxPoints(rect)
-            # box = np.int0(box)
-            # cv2.drawContours(orig_img_cp,[box],0,(0,0,255),2)
 
         contour_image = self.bridge.cv2_to_imgmsg(orig_img_cp, encoding="bgra8")
         self.contourPub.publish(contour_image)
@@ -202,7 +198,8 @@ class GateDetectionNode():
             return orig_img_cp, hull_array
         else:
             return orig_img_cp, contours_filtered
-    
+
+
     def fitlines(self, orig_img, contours):
         orig_img_cp = copy.deepcopy(orig_img)
 
@@ -215,15 +212,52 @@ class GateDetectionNode():
             lefty = int((-x*vy/vx) + y)
             righty = int(((cols-x)*vy/vx)+y)
             cv2.line(blank_image,(cols-1,righty),(0,lefty),(0,255,0),2)
-            cv2.line(orig_img_cp,(cols-1,righty),(0,lefty),(0,255,0),2)
-
-
-        fills_ros_img = self.bridge.cv2_to_imgmsg(orig_img_cp, encoding="bgra8")
-        self.fillsPub.publish(fills_ros_img)
+            #cv2.line(orig_img_cp,(cols-1,righty),(0,lefty),(0,255,0),2)
+        
+        lines_ros_image = self.bridge.cv2_to_imgmsg(blank_image, encoding="bgra8")
+        self.linesPub.publish(lines_ros_image)
 
         return blank_image
+    
+    def corner_detection(self, line_fitted_img):
+        line_fitted_img_cp = copy.deepcopy(line_fitted_img)
+        #blur_line_fitted_img = cv2.GaussianBlur(line_fitted_img_cp, (5, 19), 5.2)
+
+        blank_image = np.zeros(shape=[self.img_height, self.img_width, self.img_channels], dtype=np.uint8)
+        blank_image_corners = np.zeros(shape=[self.img_height, self.img_width, self.img_channels], dtype=np.uint8)
+
+
+        gray = cv2.cvtColor(line_fitted_img_cp,cv2.COLOR_BGR2GRAY)
+        gray = np.float32(gray)
+        dst = cv2.cornerHarris(gray,13,19,0.04)
+
+        #result is dilated for marking the corners, not important
+        dst = cv2.dilate(dst,None)
+
+        # Threshold for an optimal value, it may vary depending on the image.
+        blank_image[dst>0.00001*dst.max()]=[0,0,255,0]
+        blank_image = cv2.cvtColor(blank_image,cv2.COLOR_BGR2GRAY)
+
+        contours, hierarchy = cv2.findContours(blank_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cnt in contours:
+            cnt_moments = cv2.moments(cnt)
+
+            centroid_center_x = int(cnt_moments['m10']/cnt_moments['m00'])
+            centroid_center_y = int(cnt_moments['m01']/cnt_moments['m00'])
+
+            cv2.circle(blank_image_corners, (centroid_center_x, centroid_center_y), 2, (255,0,255), 2)
+
+        corners_ros_img = self.bridge.cv2_to_imgmsg(blank_image_corners, encoding="bgra8")
+        self.cornersPub.publish(corners_ros_img)
+
 
     def zed_callback(self, img_msg):
+        start = timer() # Start function timer.
+        #------------------------------------------>
+        #------------------------------------------>
+        #------------------------------------------>
+        
         try:
             cv_image = self.bridge.imgmsg_to_cv2(img_msg, "passthrough")
         except CvBridgeError, e:
@@ -239,9 +273,20 @@ class GateDetectionNode():
         
         hsv_mask = self.hsv_publisher(cv_image)
         noise_removed_img = self.noise_removal(hsv_mask)
-        contours_img, contours = self.contour_processing(cv_image, noise_removed_img)
+        contours_img, contours = self.contour_processing(cv_image, noise_removed_img, True)
 
-        self.fitlines(contours_img, contours)
+        line_img = self.fitlines(contours_img, contours)
+
+        self.corner_detection(line_img)
+        #------------------------------------------>
+        #------------------------------------------>
+        #------------------------------------------>
+        end = timer() # Stop function timer.
+        timediff = (end - start)
+
+        fps = 1 / timediff # Take reciprocal of the timediff to get runs per second. 
+
+        self.timerPub.publish(fps)
 
     def dynam_reconfigure_callback(self, config):
         self.canny_threshold1 = config.canny_threshold1
