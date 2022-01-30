8c0c1f392f5f850bb3ab0c2d97dde5a7a5b20cb4
Erik Rowe <ekrowe@stud.ntnu.no>
2022-01-30 20:20:10 +0100

    created node to mask the confidence map. Values over a threshold is 1 and below is 0

--- confidence_thresholding/scripts/confidence_thresholding_node.py
+++ confidence_thresholding/scripts/confidence_thresholding_node.py
@@ -4,15 +4,28 @@ import rospy
 
 # msg types
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+# classes
+from confidence_masking import ConfidenceMasking
 
 
 class ConfidenceThresholdingNode():
     rospy.init_node('confidence_thresholding_node')
-    def __init__(self) -> None:
+    def __init__(self):
         rospy.Subscriber('/zed2/zed_node/confidence/confidence_map', Image, self.confidence_cb)
+        self.bridge = CvBridge()
+        self.confMask = ConfidenceMasking()
 
     def confidence_cb(self, msg):
-        pass
+        # Bridge image data from Image to cv_image data
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(msg, "passthrough")
+        except CvBridgeError as e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
+
+        maskedMap = self.confMask.add_mask(self.cv_image, 50)
+        rospy.loginfo(maskedMap)
 
 if __name__ == '__main__':
     node = ConfidenceThresholdingNode()
