cc3fbc6aa572d056d6a791a4923a09b5355e05b3
j.langholz@gmx.de <j.langholz@gmx.de>
2022-02-27 16:00:34 +0100

    Added functions to get the bb_boxes from filtered hough lines

--- feature_detection/scripts/test_gate_detection_cv.py
+++ feature_detection/scripts/test_gate_detection_cv.py
@@ -5,15 +5,112 @@ from matplotlib import lines
 import numpy as np
 import matplotlib.pyplot as plt
 
+def lines_coord(lines,set,e):
+    '''
+    args: 
+        lines: set of lines
+        set: look for horizontal or vertical lines
+    '''
+    # set = 0: vertical lines
+    # set = 1: horizontal lines
+    if set == 0:
+        ver = 1
+    elif set ==1:
+        ver = 0
 
-print_picture = True
+    # e = 8 ## should be default value later
+    lines_sort = lines[lines[:,0,set].argsort()]  
+    x_all_lines = lines_sort[:,0,set]
+    x_position = np.zeros((20,1))
+    rect_list = np.zeros((20,1,4),dtype = int)
+
+    k= 0
+    j= 0
+    for i in range(x_all_lines.shape[0]-1):
+        if x_all_lines[i+1] - x_all_lines[i] >= e or i == x_all_lines.shape[0]-2: 
+            if len(x_all_lines[k:i]) >1:
+                x_pos_mean = int(x_all_lines[k:i+1].mean())
+                y_pos_ver =  lines_sort[k:i+1, 0, ver]
+                y_pos_min = min(y_pos_ver)
+                y_pos_ver =  lines_sort[k:i+1, 0,ver +2]
+                y_pos_max = max(y_pos_ver)
+                k = i +1
+                x_position[j] = x_pos_mean
+                rect_list[j,0,set] = int(x_pos_mean)
+                rect_list[j,0,ver] = int(y_pos_min)
+                rect_list[j,0,set+2] = int(x_pos_mean)
+                rect_list[j,0,ver+2] = int(y_pos_max)
+
+                
+                j +=1
+            else:
+                k += 1
+    return rect_list, x_position
+def cut_zeros(list):
+    beispiel = np.zeros((1,4))
+    k=0
+    original_list = list
+    for i in range(len(original_list)):
+        if np.all(original_list[i,:,:] == beispiel):
+            list = np.delete(list ,k ,axis = 0)
+            k -=1
+        k +=1
+    return list
+
+def connect_lines2bb(lines,set):
+    '''
+    args: 
+        lines: set of lines
+        set: look for horizontal or vertical lines
+    '''
+    # set = 0: vertical lines (the coordinate that is the same)
+    # set = 1: horizontal lines
+
+    ## aufgrund des Sortierens der Linien im Vorhinein ist die Berechnung der Distanz zu benachbarten Linien ausreichend
+
+    # calculating distance between lines 
+    # initialize the line vector
+    line_1 = lines[:-1,:,:]
+    line_2 = lines[1:,:,:]
+
+
+    dis = np.zeros((line_1.shape[0],1))
+    for i in range(len(line_1)): #zip(range(len(line_1)),range(len(line_2)))
+        dis[i] = line_2[i,0,set] -line_1[i,0,set]
+    # print(dis)
+    print(len(line_1))
+    bb_corner_pair = []
+    platzhalter = np.zeros((1,1,8))
+    j =0
+    for i in range(len(dis)):
+        print(len(dis)-1,i)
+        if i < len(dis)-1 and dis[i] < dis[i+1]:
+            # first pair of lines belong together
+            print(line_1[i,0,:],line_2[i,0,:])
+            platzhalter[0,0,:4] = line_1[i,0,:]
+            platzhalter[0,0,4:] = line_2[i,0,:]
+            bb_corner_pair.append(platzhalter)
+            platzhalter = np.zeros((1,1,8))
+            print(bb_corner_pair)
+        elif i == len(dis)-1 and dis[i] < dis[i-1]:
+            print(line_1[i,0,:])
+            # last pair of lines belong together
+            platzhalter[0,0,:4] = line_1[i,0,:]
+            platzhalter[0,0,4:] = line_2[i,0,:]
+            bb_corner_pair.append(platzhalter)
+            print(bb_corner_pair)
+        
+    return bb_corner_pair
+
+
+print_picture = False
 
 path1 = 'feature_detection/test_image/gate_day1_medium_yaw.png'
 path2 = 'feature_detection/test_image/gate_day1_medium_yaw_second.png'
 path3 = 'feature_detection/test_image/gate_day1_medium_yaw_third.png'
 path4 = 'feature_detection/test_image/gate_day1_medium_yaw_fourth.png'
 path= [path1, path2, path3, path4]
-for i in range(4):
+for i in range(1): # 4 for all pictures
     
     img = cv2.imread(path[i])
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -36,7 +133,7 @@ for i in range(4):
     linesP = cv2.HoughLinesP(edges,1,np.pi/180,20,50,10)
     m = np.zeros(linesP.shape[0]) ## replace this with orientation of the drone
 
-    
+    ## Orientation based Filtering 
     k = 0
     j = 0
     lines_ver = linesP
@@ -57,43 +154,32 @@ for i in range(4):
                 j -=1
             k +=1
             j +=1
-    
-    ## processing vertical lines
-    e = 8 
-    lines_ver_sort = lines_ver[lines_ver[:,0,0].argsort()]  
-    x_vertical = lines_ver_sort[:,0,0]
-    x_position = np.zeros((9,1))
-    rect_list = np.zeros((9,1,4),dtype = int)
 
+    ## processing vertical and horizontal lines
+    rect_list_ver, x_position = lines_coord(lines_ver, 0,8)
+    rect_list_hor, pos_hor = lines_coord(lines_hor, 1, 5)
 
-    k= 0
-    j= 0
-    for i in range(x_vertical.shape[0]-1):
-        if x_vertical[i+1] - x_vertical[i] >= e or i == x_vertical.shape[0]-2: 
-            if len(x_vertical[k:i]) >1:
-                x_pos_mean = int(x_vertical[k:i+1].mean())
-                y_pos_ver =  lines_ver_sort[k:i+1, 0,1]
-                y_pos_min = min(y_pos_ver)
-                y_pos_max = max(y_pos_ver)
-                k = i +1
-                x_position[j] = x_pos_mean
-                rect_list[j,0,0] = int(x_pos_mean)
-                rect_list[j,0,1] = int(y_pos_min)
-                rect_list[j,0,2] = int(x_pos_mean)
-                rect_list[j,0,3] = int(y_pos_max)
+    ## cut zeros from lists!!!
+    rect_list_ver_new = cut_zeros(rect_list_ver)
+    rect_list_hor_new = cut_zeros(rect_list_hor)
 
-                
-                j +=1
-            else:
-                k += 1
-    
+    ## correlating lines and getting corner points from bounding box
+    bb_ver = connect_lines2bb(rect_list_ver_new, 0)
+    print(bb_ver)
 
-
+    ## Visualization
     for i in range(len(x_position)):
         if x_position[i] != 0:
-            line = rect_list[i,0,:]
-            cv2.line(img, (line[0], line[1]), (line[2], line[3]), (255,255,0), 3)
+            line_ver = rect_list_ver[i,0,:]
+            cv2.line(img, (line_ver[0], line_ver[1]), (line_ver[2], line_ver[3]), (255,0,255), 3)
             # cv2.line(img, (x_position[i], 0), (x_position[i], 1000), (255,255,0), 3)
+    
+    for i in range(len(pos_hor)):
+        if pos_hor[i] != 0:
+            line_hor = rect_list_hor[i,0,:]
+            # print(line_hor)
+            cv2.line(img, (line_hor[0], line_hor[1]), (line_hor[2], line_hor[3]), (255,255,0), 4)
+            # cv2.line(img, (pos_hor[i], 0), (pos_hor[i], 1000), (255,255,0), 3)
 
     if print_picture:
         # stack_edges = np.hstack((edges,edges2))
@@ -103,8 +189,3 @@ for i in range(4):
 
 
 
-## processing vertical lines
-    # 1. ungefähre position der Pfeiler herausbekommen: 4 verschiedene x-positionen
-    # 2. obersten punkt der vertikalen Linien ermitteln 
-    # 3. obersten Punkt zum Filtern der horizontalen Linien bestimmen --> Zuverlässigkeit und Genauigkeit definieren
-    # print(lines_ver, max(lines_ver[:][0][0]))
