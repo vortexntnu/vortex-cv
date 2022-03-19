0d156b77b654e867a4f2c87e53d347001ff39a7c
Kristian Sem <krisem17@gmail.com>
2022-03-19 17:45:19 +0100

    Added function for

--- rcfa_detection/scripts/rcfa_detection_node.py
+++ rcfa_detection/scripts/rcfa_detection_node.py
@@ -26,7 +26,7 @@ class PreprocessingNode():
 
 
         # RCFA_detection
-        rospy.Subscriber('/cv/image_preprocessing/CLAHE', Image, self.image_rect_color_cb, queue_size=1)
+        rospy.Subscriber('/cv/image_preprocessing/GW', Image, self.orange_boi, queue_size=1)
         self.rcfa_pub = rospy.Publisher('/cv/preprocessing/rcfa_detection',Bool , queue_size= 1)###In order to publish bool value
 
         self.x_plot = np.linspace(0, 255., num=256)
@@ -43,11 +43,80 @@ class PreprocessingNode():
         self.ax2background = self.fig_plot.canvas.copy_from_bbox(self.ax_plot.bbox)
 
         plt.show(block=True)
+    
+    
+    def orange_boi(self, msg):
+        """
+        ***Callback***\n
+        Publishes a confident representation of the message in the topic using a confidence map.
+        Args:
+            msg: the message in the topic callback
+        """
+        cv_image = self.bridge_to_cv(msg)
+        
+        new_cfd = cv_image.copy()
+
+        # new_cfd[new_cfd < 0.3] = 0
+        # new_cfd[new_cfd > 10] = 0
+
+        hist_red = cv.calcHist([new_cfd],[2],None,[256],[0,255]) #One dimensional array. Index of array is the color intensity. The array values are pixels.
+        hist_green = cv.calcHist([new_cfd],[1],None,[256],[0,255]) #One dimensional array. Index of array is the color intensity. The array values are pixels.
+
+        hist_full = (hist_red + hist_green*0.6)/1.6
+
+                                                                    # Intensity = 0 is the highest color intensity.
+        index = np.argmax(hist_full) #Finds the index of the max element in array.
+        max = hist_full[index]  #Finds the maximum value of thr array .
+
+        mean = np.sum(hist_full)/len(hist_full) #Calculates the avarage value of pixels in the array.
+
+        
+
+
+        difference  = max-mean
+        rospy.loginfo(difference)
+        #if (difference>20000):
+        #    rospy.loginfo("GATE DETECTED")
+        #    #rospy.loginfo(difference)
+        #    gateDetection = True
+        #    self.rcfa_pub.publish(gateDetection) #Publishes gateDetection 
+
+
+        #else:
+        #    rospy.loginfo("NO GATE")
+        #    gateDetection = False
+        #    self.rcfa_pub.publish(gateDetection)
+
+
+        #hist_not_full = hist_full[:100]
+        
+        #thresh = np.sum(hist_not_full)
+        #rospy.loginfo(thresh)
+        
+        
+        
+        
+        
+        self.line_plot.set_data(self.x_plot, hist_full)
+        # restore background
+        self.fig_plot.canvas.restore_region(self.ax2background)
+
+        # redraw just the points
+        self.ax_plot.draw_artist(self.line_plot)
+
+        # fill in the axes rectangle
+        self.fig_plot.canvas.blit(self.ax_plot.bbox)
+
+        # in this post http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
+        # it is mentionned that blit causes strong memory leakage. 
+        # however, I did not observe that.
+
+        self.fig_plot.canvas.flush_events()        
 
     def image_rect_color_cb(self, msg):
         """
         ***Callback***\n
-        Pyblishes a confident representation of the message in the topic using a confidence map.
+        Publishes a confident representation of the message in the topic using a confidence map.
         Args:
             msg: the message in the topic callback
         """
