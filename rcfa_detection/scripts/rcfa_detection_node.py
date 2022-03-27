745fabb6715860a168e8bfabbfe2b6ce43da0543
Henrik Hammarstrøm <henrhamm@stud.ntnu.no>
2022-03-27 20:30:44 +0200

    Cleaned up rcfa_detection_node.py

--- rcfa_detection/scripts/rcfa_detection_node.py
+++ rcfa_detection/scripts/rcfa_detection_node.py
@@ -4,99 +4,64 @@ from xmlrpc.client import Boolean
 import rospy
 
 # msg types
-from sensor_msgs.msg import Image, PointCloud2
-from std_msgs.msg import Float32MultiArray, Bool
+from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
 
-class PreprocessingNode():
+class ncfaDetectionNode():
     """
-    Class to handle operations related to the preprocessing node. \n
-    This includes:
-        - Confidence map --> masked confidence map
-        - and so on !!!!!!!!!!!!!! So on is kind of unspecific
+    Class to plot histograms and detect objects based on color frequency.
     """
-    def __init__(self):
+    def __init__(self, do_plot = True, mode = 1):
+        '''
+        parms: 
+            do_plot (bool) : bool value to determine if we will plot a histogarm
+            mode (int) : Will determine what what channel we will inspect 
+            if mode = 1: rcfa activated
+            if mode = 2: ocfa activated
+        
+        '''
         rospy.init_node('rcfa_detection')
 
         self.bridge = CvBridge()
+        self.bool = do_plot
+        self.mode = mode
 
+        if(mode==1):
+            rospy.Subscriber('/cv/image_preprocessing/GW', Image, self.rcfa_cb, queue_size=1)
+            self.rcfa_pub = rospy.Publisher('/cv/preprocessing/rcfa_detection',Bool , queue_size= 1)###In order to publish bool value
 
-        # RCFA_detection
-        rospy.Subscriber('/cv/image_preprocessing/GW', Image, self.image_rect_color_cb, queue_size=1)
-        self.rcfa_pub = rospy.Publisher('/cv/preprocessing/rcfa_detection',Bool , queue_size= 1)###In order to publish bool value
+        elif(mode == 2):
+            rospy.Subscriber('/cv/image_preprocessing/GW', Image, self.ocfa_cb, queue_size=1)
+            self.ocfa_pub = rospy.Publisher('/cv/preprocessing/ocfa_detection',Bool , queue_size= 1)
 
-        # self.x_plot = np.linspace(0, 255., num=256)
-        # self.fig_plot = plt.figure()
-        # self.ax_plot = self.fig_plot.add_subplot(1, 1, 1)
+        #Plotting the figure for histogram
+        if(self.bool):
+            self.x_plot = np.linspace(0, 255., num=256)
+            self.fig_plot = plt.figure()
+            self.ax_plot = self.fig_plot.add_subplot(1, 1, 1)
 
-        # self.line_plot, = self.ax_plot.plot([], lw=3)
+            self.line_plot, = self.ax_plot.plot([], lw=3)
 
-        # self.ax_plot.set_xlim(self.x_plot.min(), self.x_plot.max())
-        # self.ax_plot.set_ylim([0, 100000])
+            self.ax_plot.set_xlim(self.x_plot.min(), self.x_plot.max())
+            self.ax_plot.set_ylim([0, 100000])
 
-        # self.fig_plot.canvas.draw()   # note that the first draw comes before setting data 
+            self.fig_plot.canvas.draw()   # note that the first draw comes before setting data 
 
-        # self.ax2background = self.fig_plot.canvas.copy_from_bbox(self.ax_plot.bbox)
+            self.ax2background = self.fig_plot.canvas.copy_from_bbox(self.ax_plot.bbox)
 
-        # plt.show(block=True)
+            plt.show(block=True)
     
-    
-    def orange_boi(self, msg):
+    def do_plot(self,hist_full):
         """
-        ***Callback***\n
-        Publishes a confident representation of the message in the topic using a confidence map.
-        Args:
-            msg: the message in the topic callback
+        Params:
+            hist_full   (numpy.ndarray) : #One dimensional array. Index of array is the color intensity. The array values are pixels.
+        
         """
-        cv_image = self.bridge_to_cv(msg)
-        
-        new_cfd = cv_image.copy()
-
-        # new_cfd[new_cfd < 0.3] = 0
-        # new_cfd[new_cfd > 10] = 0
-
-        hist_red = cv.calcHist([new_cfd],[2],None,[256],[0,255]) #One dimensional array. Index of array is the color intensity. The array values are pixels.
-        hist_green = cv.calcHist([new_cfd],[1],None,[256],[0,255]) #One dimensional array. Index of array is the color intensity. The array values are pixels.
-
-        hist_full = (hist_red + hist_green*0.6)/1.6
-
-                                                                    # Intensity = 0 is the highest color intensity.
-        index = np.argmax(hist_full) #Finds the index of the max element in array.
-        max = hist_full[index]  #Finds the maximum value of thr array .
-
-        mean = np.sum(hist_full)/len(hist_full) #Calculates the avarage value of pixels in the array.
-
-        
-
-
-        difference  = max-mean
-        rospy.loginfo(difference)
-        #if (difference>20000):
-        #    rospy.loginfo("GATE DETECTED")
-        #    #rospy.loginfo(difference)
-        #    gateDetection = True
-        #    self.rcfa_pub.publish(gateDetection) #Publishes gateDetection 
-
-
-        #else:
-        #    rospy.loginfo("NO GATE")
-        #    gateDetection = False
-        #    self.rcfa_pub.publish(gateDetection)
-
-
-        #hist_not_full = hist_full[:100]
-        
-        #thresh = np.sum(hist_not_full)
-        #rospy.loginfo(thresh)
-        
-        
-        
-        
-        
         self.line_plot.set_data(self.x_plot, hist_full)
         # restore background
         self.fig_plot.canvas.restore_region(self.ax2background)
@@ -106,80 +71,88 @@ class PreprocessingNode():
 
         # fill in the axes rectangle
         self.fig_plot.canvas.blit(self.ax_plot.bbox)
+        
+        self.fig_plot.canvas.flush_events()
 
-        # in this post http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
-        # it is mentionned that blit causes strong memory leakage. 
-        # however, I did not observe that.
 
-        self.fig_plot.canvas.flush_events()        
-
-    def image_rect_color_cb(self, msg):
+    def ocfa_cb(self, msg, threshold_o = 30000):
         """
-        ***Callback***\n
-        Publishes a confident representation of the message in the topic using a confidence map.
-        Args:
-            msg: the message in the topic callback
+          ***Callback***\n
+        Publishes a boolean value if the amount of red pixels is higher than the threshhold.
+        Params:
+            msg (sensor_msgs/Image) :The message in the topic callback.
+            threshold_o (int)   :Threshold for the frequency detection.
         """
         cv_image = self.bridge_to_cv(msg)
         
         new_cfd = cv_image.copy()
 
-        # new_cfd[new_cfd < 0.3] = 0
-        # new_cfd[new_cfd > 10] = 0
+        hist_red = cv.calcHist([new_cfd],[2],None,[256],[0,255]) 
+        hist_green = cv.calcHist([new_cfd],[1],None,[256],[0,255]) 
 
-        hist_full = cv.calcHist([new_cfd],[2],None,[256],[0,255]) #One dimensional array. Index of array is the color intensity. The array values are pixels.
-                                                                    # Intensity = 0 is the highest color intensity.
-        index = np.argmax(hist_full) #Finds the index of the max element in array.
-        max = hist_full[index]  #Finds the maximum value of thr array .
+        hist_full = (hist_red + hist_green*0.6)/1.6
 
-        mean = np.sum(hist_full)/len(hist_full) #Calculates the avarage value of pixels in the array.
-
-        
-
+                                                                    
+        index = np.argmax(hist_full) 
+        max = hist_full[index]  
+        mean = np.sum(hist_full)/len(hist_full) 
 
         difference  = max-mean
         rospy.loginfo(difference)
-        if (difference>30000):
+        if (difference>threshold_o):
             rospy.loginfo("GATE DETECTED")
             gateDetection = True
             self.rcfa_pub.publish(gateDetection) #Publishes gateDetection 
-
 
         else:
             rospy.loginfo("NO GATE")
             gateDetection = False
             self.rcfa_pub.publish(gateDetection)
 
+        if (self.bool):
+            self.do_plot(hist_full)       
 
-        #hist_not_full = hist_full[:100]
-        
-        #thresh = np.sum(hist_not_full)
-        #rospy.loginfo(thresh)
-        
-        
-        
-        
-        
-        # self.line_plot.set_data(self.x_plot, hist_full)
-        # # restore background
-        # self.fig_plot.canvas.restore_region(self.ax2background)
+    def rcfa_cb(self, msg, threshold_r = 30000):
+        """
+        ***Callback***\n
+        Publishes a boolean value if the amount of red pixels is higher than the threshhold.
+        Params:
+            msg (sensor_msgs/Image) :The message in the topic callback.
+            threshold_r (int)   :Threshold for the frequency detection.
+        """
+        cv_image = self.bridge_to_cv(msg)
+        new_cfd = cv_image.copy()
 
-        # # redraw just the points
-        # self.ax_plot.draw_artist(self.line_plot)
+        hist_full = cv.calcHist([new_cfd],[2],None,[256],[0,255])                                                     
+        index = np.argmax(hist_full)
+        max = hist_full[index] 
+        mean = np.sum(hist_full)/len(hist_full)
+        difference  = max-mean
 
-        # # fill in the axes rectangle
-        # self.fig_plot.canvas.blit(self.ax_plot.bbox)
+        rospy.loginfo(difference)
+        if (difference>threshold_r):
+            rospy.loginfo("GATE DETECTED")
+            gateDetection = True
+            self.rcfa_pub.publish(gateDetection) #Publishes gateDetection 
 
-        # # in this post http://bastibe.de/2013-05-30-speeding-up-matplotlib.html
-        # # it is mentionned that blit causes strong memory leakage. 
-        # # however, I did not observe that.
+        else:
+            rospy.loginfo("NO GATE")
+            gateDetection = False
+            self.rcfa_pub.publish(gateDetection)
 
-        # self.fig_plot.canvas.flush_events()
-
+        if (self.bool):
+            self.do_plot(hist_full)
+        
     def bridge_to_cv(self, image_msg, encoding = "passthrough"):
-        """This function returns a cv image from a ros image"""
-        # !!!!!!!!!!!!!! comments on arguments are missing
-        # Bridge image data from Image to cv_image data
+        """This function returns a cv image from a ros image
+        
+        Params:
+            image_msg: [sensor_msgs::Image message] Input image in ros format
+            encoding: [str] (default = passtrough) The encoding of the image data.
+        Return:
+            image_transformed: [cv::Mat] Output image in OpenCV format
+        """
+        
         image_transformed = None
         try:
             image_transformed = self.bridge.imgmsg_to_cv2(image_msg, encoding)
@@ -188,7 +161,15 @@ class PreprocessingNode():
         return image_transformed
 
     def bridge_to_image(self, cv_image_msg, encoding = "passthrough"):
-        """This function returns a ros image from a cv image"""
+        """This function returns a ros image from a cv image
+            
+        Params:
+            cv_image_msg: [cv::Image] Input image in cv format
+            encoding: [str] (default = passtrough) The encoding of the image data.
+        Return:
+            image_transformed: [sensor_msgs::Image message] Output image in ros format
+        """
+        
         # comments on arguments are missing
         # Bridge image data from CV image to Image data
         image_transformed = None
@@ -200,7 +181,7 @@ class PreprocessingNode():
         
 
 if __name__ == '__main__':
-    node = PreprocessingNode()
+    node = ncfaDetectionNode()
 
     while not rospy.is_shutdown():
         rospy.spin()
