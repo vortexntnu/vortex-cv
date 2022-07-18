81ed3afa2b068ff828c4a49261cade423028bbeb
Kristian Sem <krisem17@gmail.com>
2022-07-18 13:18:53 +0200

    Added code for launching vision_launch.launch in the shell file. This file doesn't contain darknet launch

--- launch.sh
+++ launch.sh
@@ -3,3 +3,7 @@
 # The purpose of this file is to automate launching of 
 # all nodes found in this repository FOR ROBOSUB 2022
 
+cd ~/cv_ws
+source devel/setup.bash
+roslaunch vision_launch vision_launch.launch
+
