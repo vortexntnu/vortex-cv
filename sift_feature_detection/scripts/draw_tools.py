8ccafbd69e87bf71c2ad7e107cc0843f1549f7dd
Kristian Sem <krisem17@gmail.com>
2022-07-20 10:15:03 +0200

    Moved visaulization tools to another file and cleanup

---
+++ sift_feature_detection/scripts/draw_tools.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import cv2 as cv
+import numpy as np
+
+class DrawTools:
+
+    def __init__(self):
+
+        hello = 1
+
+    def find_centeroid(self, dst):
+        '''
+        Finds the centeroid for a trapezoid.
+        Taking in four corner points and finding the middle points between these.
+        The middle points then gets utillized to find the center of the figure using these.
+
+        p1----p14----p4  
+        |             |
+        |             |
+       p12     c     p34
+        |             |
+        |             |
+        p2----p23-----p3
+
+        Args:
+            Four cornerpoints in a (4,1,2) array
+
+        Returns:
+            The centeroid points as a (1,2) array
+        '''
+
+        points = np.reshape(dst, (4,2))
+        x_c = np.average(points[:,0])
+        y_c = np.average(points[:,1])
+        center = np.array([x_c, y_c])
+
+        return center
+        
+    def drawcircle(self, img, center_coordinates=(10, 20), color=(255, 0, 0), radius=5, thickness=2):
+        img = cv.circle(img, center_coordinates, radius, color, thickness)
+        return img
+
+    def draw_all_circles(self, img, dst, radius):
+
+        points = np.reshape(dst, (4,2))
+        self.drawcircle(img, center_coordinates=(points[0][0], points[0][1]), color=(255, 0, 0),     radius=radius)
+        self.drawcircle(img, center_coordinates=(points[1][0], points[1][1]), color=(0, 255, 0),     radius=radius)
+        self.drawcircle(img, center_coordinates=(points[2][0], points[2][1]), color=(0, 0, 255),     radius=radius)
+        self.drawcircle(img, center_coordinates=(points[3][0], points[3][1]), color=(255, 255, 255), radius=radius)
+
+        return img
+
+    def text_on_image(self, img, dst, image_number):
+        points = np.reshape(dst, (4,2))
+        x, y = int(points[0][0]) ,int(points[0][1] - 15.0)
+        h,w = img.shape[0], img.shape[1]
+        text = "image" + str(image_number)
+
+        cv.putText(img, text, (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2, cv.LINE_AA)
+
+        return img
+
+    def draw_all(self, img, dst, dst_packed, image_number,centeroid=False):
+        if centeroid == True:
+            centeroid  = self.find_centeroid(dst)
+        
+        img = self.draw_all_circles(img, dst, radius=8)
+        img = self.draw_all_circles(img, dst_packed, radius=5)
+        img = self.text_on_image(img, dst, image_number)
+        img = self.drawcircle(img, center_coordinates=(centeroid[0], centeroid[1]))
+        img = cv.polylines(img,[np.int32(dst)],True,255,3, cv.LINE_AA)
+
+        return img
+
+    # def publish_centeroid(self, img_numb, centeroid, orientation):
+    #     pub = PoseStamped()
+    #     pub.header.frame_id = "image" + str(img_numb)
+    #     pub.pose.position.x = centeroid[0]
+    #     pub.pose.position.y = centeroid[1]
+    #     pub.pose.position.z = 0
+    #     pub.pose.orientation.x = orientation[0]
+    #     pub.pose.orientation.y = orientation[1]
+    #     pub.pose.orientation.z = orientation[2]
+    #     pub.pose.orientation.w = orientation[3]
+
+    #     self.detection_centeroid_pub.publish(pub)
--- sift_feature_detection/scripts/sift_node.py
+++ sift_feature_detection/scripts/sift_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from operator import imod
 import cv2 as cv
 from cv_bridge import CvBridge, CvBridgeError
 import glob
@@ -12,6 +13,10 @@ import tf.transformations
 from sensor_msgs.msg import Image
 from cv_msgs.msg import Point2, PointArray
 from geometry_msgs.msg import PoseStamped
+
+# Contains visualization tools
+from draw_tools import DrawTools
+
 
 class SiftFeature:
     '''
@@ -31,24 +36,6 @@ class SiftFeature:
     '''
     
     def __init__(self):
-        ##################
-        ##### Params #####
-        ##################
-
-        # Image rate 
-        self.get_image_rate = 0.1
-
-        #  grayscale = 0, color = 1
-        self.colormode = 1
-
-        # Compare image(s)
-        self.image_list = [cv.imread(file, self.colormode) for file in glob.glob("/home/vortex/cv_ws/src/Vortex-CV/sift_feature_detection/data/sift_images/*.png")]
-
-        # Minimum matches needed for drawing bounding box
-        self.MIN_MATCH_COUNT = 8
-
-        # Scale the bounding box (Only downscale)
-        self.scale = 0.999
 
         #################
         ###### Node #####
@@ -71,20 +58,47 @@ class SiftFeature:
         self.cornerpoints_pub = rospy.Publisher('/feature_detection/sift_object_points', PointArray, queue_size= 1)
         self.detection_centeroid_pub = rospy.Publisher('/feature_detection/sift_detection_centeroid', PoseStamped, queue_size=1)
 
+        ##################
+        ##### Params #####
+        ##################
+
+        # Image rate 
+        self.get_image_rate = 0.1
+
+        #  grayscale = 0, color = 1
+        self.colormode = 1
+
+        # Image path
+        path = "/home/vortex/cv_ws/src/Vortex-CV/sift_feature_detection/data/sift_images/*.png"
+
+        # Compare image(s)
+        self.image_list = [cv.imread(file, self.colormode) for file in glob.glob(path)]
+        rospy.loginfo("Number of images: %s", len(self.image_list))
+        if len(self.image_list) == 0:
+            rospy.logwarn("\n ######################################## \n ### No images found! Check your path!### \n ########################################")
+
+        # Minimum matches needed for drawing bounding box
+        self.MIN_MATCH_COUNT = 8
+
+        # Scale the bounding box (Only downscale)
+        self.scale = 0.999
+
         ################
         ###CV stuff ####
         ################
+        self.drawtools = DrawTools()
+
         self.bridge = CvBridge() 
 
         # Initiate SIFT detector (opencv version 3.4.x) 
-        self.sift = cv.xfeatures2d.SIFT_create()
+        #self.sift = cv.xfeatures2d.SIFT_create()
         # (opencv version 4.5.1)
-        #self.sift = cv.SIFT_create()
+        self.sift = cv.SIFT_create()
 
         self.kp = []
         self.des = []
 
-        rospy.loginfo("Number of images: %s", len(self.image_list))
+        
 
         # Gets keypoints and descriptors for every loaded image
         for image in self.image_list:
@@ -96,33 +110,6 @@ class SiftFeature:
         ############
         ##Init end##
         ############
-
-    def drawcircle(self, image, center_coordinates=(10, 20), color=(255, 0, 0), radius=5, thickness=2):
-        image = cv.circle(image, center_coordinates, radius, color, thickness)
-        return image 
-
-    def draw_all_circles(self, dst, radius):
-
-        points = np.reshape(dst, (4,2))
-        self.drawcircle(self.cv_image, center_coordinates=(points[0][0], points[0][1]), color=(255, 0, 0),     radius=radius)
-        self.drawcircle(self.cv_image, center_coordinates=(points[1][0], points[1][1]), color=(0, 255, 0),     radius=radius)
-        self.drawcircle(self.cv_image, center_coordinates=(points[2][0], points[2][1]), color=(0, 0, 255),     radius=radius)
-        self.drawcircle(self.cv_image, center_coordinates=(points[3][0], points[3][1]), color=(255, 255, 255), radius=radius)
-
-    def text_on_image(self, img, dst, image_number):
-        points = np.reshape(dst, (4,2))
-        x, y = int(points[0][0]) ,int(points[0][1] - 15.0)
-        h,w = img.shape[0], img.shape[1]
-        text = "image" + str(image_number)
-
-        cv.putText(img, text, (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2, cv.LINE_AA)
-
-    def draw_things(self, img, dst, dst_packed, centeroid, image_number):
-        self.draw_all_circles(dst, radius=8)
-        self.draw_all_circles(dst_packed, radius=5)
-        self.text_on_image(self.cv_image, dst, image_number)
-        self.drawcircle(self.cv_image, center_coordinates=(centeroid[0], centeroid[1]))
-        self.cv_image = cv.polylines(self.cv_image,[np.int32(dst)],True,255,3, cv.LINE_AA)
 
     def scale_bounding_box(self, dst):
         '''
@@ -161,34 +148,6 @@ class SiftFeature:
 
         return np.reshape(dst_scaled,(4,1,2)), dst_scaled
 
-    def find_centeroid(self, dst):
-        '''
-        Finds the centeroid for a trapezoid.
-        Taking in four corner points and finding the middle points between these.
-        The middle points then gets utillized to find the center of the figure using these.
-
-        p1----p14----p4  
-        |             |
-        |             |
-       p12     c     p34
-        |             |
-        |             |
-        p2----p23-----p3
-
-        Args:
-            Four cornerpoints in a (4,1,2) array
-
-        Returns:
-            The centeroid points as a (1,2) array
-        '''
-
-        points = np.reshape(dst, (4,2))
-        x_c = np.average(points[:,0])
-        y_c = np.average(points[:,1])
-        center = np.array([x_c, y_c])
-
-        return center
-
     def publish_point_array_msg(self, point_array, obj_class, image_width, image_height):
         pt_arr_msg = PointArray()
 
@@ -210,37 +169,8 @@ class SiftFeature:
         
         self.cornerpoints_pub.publish(pt_arr_msg)
 
-    def publish_centeroid(self, img_numb, centeroid, orientation):
-        pub = PoseStamped()
-        pub.header.frame_id = "image" + str(img_numb)
-        pub.pose.position.x = centeroid[0]
-        pub.pose.position.y = centeroid[1]
-        pub.pose.position.z = 0
-        pub.pose.orientation.x = orientation[0]
-        pub.pose.orientation.y = orientation[1]
-        pub.pose.orientation.z = orientation[2]
-        pub.pose.orientation.w = orientation[3]
+    def compare_matches(self, cam_image, flann, kp2, des2):
 
-        self.detection_centeroid_pub.publish(pub)
-
-
-
-    def callback(self, img_in):
-        try:
-            self.cv_image = self.bridge.imgmsg_to_cv2(img_in, "passthrough")
-            if self.colormode == 0:
-                self.cv_image = cv.cvtColor(self.cv_image, cv.COLOR_BGR2GRAY)      
-        except CvBridgeError, e:
-            rospy.logerr("CvBridge Error: {0}".format(e))
-
-        #rospy.loginfo("image recived")
-
-        kp2, des2 = self.sift.detectAndCompute(self.cv_image,None)
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-        search_params = dict(checks = 50)
-        flann = cv.FlannBasedMatcher(index_params, search_params)
-        
         for i in range(len(self.image_list)):
             matches = flann.knnMatch(self.des[i],des2,k=2)
 
@@ -265,32 +195,48 @@ class SiftFeature:
                 dst = cv.perspectiveTransform(pts,M)
 
                 # Gets orientation of the object
-                M0 = np.zeros((4,4))
-                M0[:3, :3] = M[:3, :3]
-                M0[3,3] = 1
-                orientation = tf.transformations.quaternion_from_matrix(M0)
+                #M0 = np.zeros((4,4))
+                #M0[:3, :3] = M[:3, :3]
+                #M0[3,3] = 1
+                #orientation = tf.transformations.quaternion_from_matrix(M0)
 
                 # Scales the bounding box
                 dst_scaled_cv_packed, dst_scaled = self.scale_bounding_box(dst)
                 
-                #Finds the centeroid of the bounding box
-                centeroid = self.find_centeroid(dst_scaled)
-
                 #self.publish_centeroid(i, centeroid, orientation)
-                self.publish_point_array_msg(dst_scaled, "image" + str(i), self.cv_image.shape[1], self.cv_image.shape[0])
+                self.publish_point_array_msg(dst_scaled, "image" + str(i), cam_image.shape[1], cam_image.shape[0])
 
                 # Only for visual effects (draws bounding box, cornerpoints, etc...)
-                self.draw_things(self.cv_image, dst, dst_scaled_cv_packed, centeroid, i)
+                cam_image = self.drawtools.draw_all(cam_image, dst, dst_scaled_cv_packed, i, centeroid=True)
 
             else:
                 #print( "Not enough matches are found - {}/{}".format(len(good), self.MIN_MATCH_COUNT) )
                 matchesMask = None
 
+        return cam_image
+
+    def callback(self, cam_image_ros):
+        try:
+            cam_image = self.bridge.imgmsg_to_cv2(cam_image_ros, "passthrough")
+            if self.colormode == 0:
+                cam_image = cv.cvtColor(cam_image, cv.COLOR_BGR2GRAY)      
+        except CvBridgeError, e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
+
+        kp2, des2 = self.sift.detectAndCompute(cam_image, None)
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        
+        cam_image = self.compare_matches(cam_image, flann, kp2, des2)
+
         if self.colormode == 0:
-            pub_img = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="mono8")
+            pub_img = self.bridge.cv2_to_imgmsg(cam_image, encoding="mono8")
         else:
-            pub_img = self.bridge.cv2_to_imgmsg(self.cv_image, encoding="bgra8")
+            pub_img = self.bridge.cv2_to_imgmsg(cam_image, encoding="bgra8")
         self.detections_pub.publish(pub_img)
+
         rospy.sleep(self.get_image_rate)
         
 
