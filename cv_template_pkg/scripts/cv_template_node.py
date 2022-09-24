9ffa591c88b71bf81c7e732b61595b45caeb2ee5
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2022-09-24 11:09:09 +0200

    Create CV processing core/example node. (#85)
    * Create CV processing core/example node.
    
    * rename to 'cv template'
    
    Co-authored-by: BenG <trooper@theEmpire.com>

---
+++ cv_template_pkg/scripts/cv_template_node.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+
+import dynamic_reconfigure.client
+
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 as cv
+import numpy as np
+import traceback
+
+class CVTemplateNode():
+    """Handles tasks related to feature detection
+    """
+
+    def __init__(self, image_topic):
+        rospy.init_node('cv_template_node')
+        
+        self.ros_rate = rospy.Rate(60.0)
+
+        self.imgSub = rospy.Subscriber(image_topic, Image, self.img_callback)
+        self.imgPub = rospy.Publisher('/cv_test/test', Image, queue_size= 1)
+
+        self.bridge = CvBridge()
+
+        # test params
+        self.test_param = 100
+        self.test_enum = 3
+        self.min_max_params = [0,
+                               179,
+                               0,
+                               255,
+                               0,
+                               255]
+        
+        # First initialization of image shape
+        first_image_msg = rospy.wait_for_message(image_topic, Image)
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(first_image_msg, "passthrough")
+            self.image_shape = self.cv_image.shape
+        except CvBridgeError, e:
+            raise("CV Bridge was not successful in converting the ROS image...")
+
+        self.dynam_client = dynamic_reconfigure.client.Client("/test_cfg/cv_template_cfg", config_callback=self.dynam_reconfigure_callback)
+
+    def cv_image_publisher(self, publisher, image, msg_encoding="bgra8"):
+        """
+        Takes a cv::Mat image object, converts it into a ROS Image message type, and publishes it using the specified publisher.
+        """
+        msgified_img = self.bridge.cv2_to_imgmsg(image, encoding=msg_encoding)
+        publisher.publish(msgified_img)
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            if self.cv_image is not None:
+                try:
+                    gray = np.zeros(shape=self.cv_image.shape)
+                    if self.cv_image.shape[2] == 3:
+                        gray = cv.cvtColor(self.cv_image, cv.COLOR_BGR2GRAY)
+                    else:
+                        gray = cv.cvtColor(self.cv_image, cv.COLOR_BGRA2GRAY)
+
+                    self.cv_image_publisher(self.imgPub, gray, msg_encoding="8UC1")
+
+                except Exception, e:
+                    rospy.logwarn(traceback.format_exc())
+                    rospy.logwarn(rospy.get_rostime())
+
+            self.ros_rate.sleep()
+
+    def img_callback(self, img_msg):
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(img_msg, "passthrough")
+        except CvBridgeError, e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
+
+    def dynam_reconfigure_callback(self, config):
+        # Slider Example
+        self.test_param = config.test_slider1
+
+        # Enum example
+        self.test_enum = config.test_enum
+
+        # Min Max slider examples
+        self.min_max_params[0] = config.test_min1
+        self.min_max_params[1] = config.test_max1
+        self.min_max_params[2] = config.test_min2
+        self.min_max_params[3] = config.test_max2
+        self.min_max_params[4] = config.test_min3
+        self.min_max_params[5] = config.test_max3
+
+if __name__ == '__main__':
+    try:
+        cv_template_node = CVTemplateNode(image_topic='/zed2/zed_node/rgb_raw/image_raw_color')
+        # rospy.spin()
+        cv_template_node.spin()
+
+    except rospy.ROSInterruptException:
+        pass
---
+++ cv_template_pkg/scripts/dynam_recfg_core.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import rospy
+from ddynamic_reconfigure_python.ddynamic_reconfigure import DDynamicReconfigure
+
+class DynamicReconfigure(object):
+    def __init__(self, *args, **kwargs):        
+        self.ddr = DDynamicReconfigure("cv_template_cfg")
+
+        # Slider example
+        self.ddr.add_variable("test_slider1", "test",   100,    0,  1000)
+        
+        
+        # Enum example
+        enum_test = self.ddr.enum([ self.ddr.const("Small", "int", 3, "A small constant"),
+                            self.ddr.const("Medium", "int", 5, "A medium constant"),
+                            self.ddr.const("Large", "int", 7, "A large constant")],
+                            "test")
+        self.ddr.add_variable("test_enum", "A size parameter which is edited via an enum", 3, 3, 7, edit_method=enum_test)
+
+        # Min Max slider examples
+        self.ddr.add_variable("test_min1", "PLACEHOLDER",   179,    0,  179)
+        self.ddr.add_variable("test_max1", "PLACEHOLDER",   0,    0,  179)
+        self.ddr.add_variable("test_min2", "PLACEHOLDER",   255,    0,  255)
+        self.ddr.add_variable("test_max2", "PLACEHOLDER",   0,    0,  255)
+        self.ddr.add_variable("test_min3", "PLACEHOLDER",   255,    0,  255)
+        self.ddr.add_variable("test_max3", "PLACEHOLDER",   0,    0,  255)
+
+        self.add_variables_to_self()
+        self.ddr.start(self.dyn_rec_callback)
+
+        super(DynamicReconfigure, self).__init__(*args, **kwargs)
+
+    def add_variables_to_self(self):
+        var_names = self.ddr.get_variable_names()
+        for var_name in var_names:
+            self.__setattr__(var_name, None)
+
+    def dyn_rec_callback(self, config, level):
+        rospy.loginfo("Received reconf call: " + str(config))
+        
+        # Update all variables
+        var_names = self.ddr.get_variable_names()
+        for var_name in var_names:
+            self.__dict__[var_name] = config[var_name]
+        return config
+
+if __name__ == '__main__':
+    rospy.init_node('cv_template_cfg')
+
+    # Create a D(ynamic)DynamicReconfigure
+    dynam = DynamicReconfigure()
+
+    rospy.spin()
