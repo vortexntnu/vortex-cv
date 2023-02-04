4f555c7c3b4de20898605889f075053704ddfea7
Eirik Kolås <kolaseirik@gmail.com>
2023-02-04 01:43:35 +0100

    node and dependencies build successfully

--- aruco_detection/include/aruco_detection_node.h
+++ aruco_detection/include/aruco_detection_node.h
@@ -1,8 +1,6 @@
-#include "ros/ros.h"
-#include "geometry_msgs/Point.h"
-#include <vortex_msgs/ObjectPosition.h>
-#include <map>
-#include <string>
+#include <ros/ros.h>
+#include <opencv2/aruco.hpp>
+#include <sensor_msgs/Image.h>
 
 class ArucoDetectionNode{
     /**
@@ -12,27 +10,29 @@ class ArucoDetectionNode{
      * before they are published on a ROS-topic which the control system is subscribed to. 
      */
 public:
-    class ArucoDetectionNode();
+    ArucoDetectionNode();
     /**
      * The callback function for the op_sub-subscriber.
      * @param objPos is the message received on the ROS-topic, containing an object ID and the position of the object.
      * The object ID and position is stored in the objectPositions-map
      * The message received is further published on the object_positions_out-topic.
      */
-    void callback(vortex_msgs::ObjectPosition objPos);
+void callback(const sensor_msgs::ImageConstPtr& img_source);
+
     /**
      * ros::spinOnce() is called at 10Hz
      */
     void execute();
-    /**
-     * Prints the contents of the objectPositions-map
-     */
-    void printMap(std::map<std::string,geometry_msgs::Point> objectsMap);
+
 
 protected:
     ros::NodeHandle node;
     ros::Subscriber op_sub;
     ros::Publisher op_pub;
     ros::Rate loop_rate;
-    std::map<std::string,geometry_msgs::Point> objectPositions;
+
+    // Dictionary of accepted ArUco marker-IDs
+    cv::Ptr<cv::aruco::Dictionary> dictionary;
+    // Parameters for camera calibration
+    cv::Ptr<cv::aruco::DetectorParameters> parameters;
 };
