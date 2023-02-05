688f3c705b45dd910cfb351e59faf32c52e5f3a0
Eirik Kolås <kolaseirik@gmail.com>
2023-02-05 19:28:21 +0100

    working NOT

--- aruco_detection/include/aruco_detection_node.h
+++ aruco_detection/include/aruco_detection_node.h
@@ -1,8 +1,14 @@
 #include <ros/ros.h>
 #include <opencv2/aruco.hpp>
 #include <sensor_msgs/Image.h>
+#include <aruco_ros>
+#include <aruco_msgs/MarkerArray.h>
+#include <cv_bridge/cv_bridge.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <vortex_msgs/LandmarkPose.h>
+#include <vector>
 
-class ArucoDetectionNode{
+class ArucoDetectionNode {
     /**
      * To serve as an interface between the perception system and the control system,
      * an instance of this class receives object positions ("landmarks") published by 
@@ -17,22 +23,36 @@ public:
      * The object ID and position is stored in the objectPositions-map
      * The message received is further published on the object_positions_out-topic.
      */
-void callback(const sensor_msgs::ImageConstPtr& img_source);
+    void callback(const sensor_msgs::ImageConstPtr& img_source);
 
     /**
      * ros::spinOnce() is called at 10Hz
      */
     void execute();
 
+    void estimateMarkerPoses(
+        std::vector<int> markerIds,
+        std::vector<std::vector<cv::Point2f>> markerCorners,
+        aruco_msgs::MarkerArray& markerMsg);
+
+    void estimateBoardPose(
+        std::vector<int> markerIds,
+        std::vector<std::vector<cv::Point2f>> markerCorners, 
+        geometry_msgs::Pose boardPose);
+
 
 protected:
     ros::NodeHandle node;
+    ros::Rate loop_rate;
     ros::Subscriber op_sub;
     ros::Publisher op_pub;
-    ros::Rate loop_rate;
 
     // Dictionary of accepted ArUco marker-IDs
-    cv::Ptr<cv::aruco::Dictionary> dictionary;
+    const cv::Ptr<cv::aruco::Dictionary> dictionary;
     // Parameters for camera calibration
-    cv::Ptr<cv::aruco::DetectorParameters> parameters;
+    const cv::Ptr<cv::aruco::DetectorParameters> detectorParameters;
+    const cv::Mat cameraMatrix;
+    const cv::Mat distCoeffs;
+    const float   markerLength;
+    const cv::aruco::Board board;
 };
--- aruco_detection/src/aruco_detection_node.cpp
+++ aruco_detection/src/aruco_detection_node.cpp
@@ -1,17 +1,16 @@
 #include "aruco_detection_node.h"
 
-#include <MarkerArray.h>
-#include <cv_bridge/cv_bridge.h>
-#include <vector>
 
-
-ArucoDetectionNode::ArucoDetectionNode() : loop_rate(10) {
-    op_sub = node.subscribe("/zed2/zed_node/rgb/image_rect_color",10, &ArucoDetectionNode::callback, this);
-
+ArucoDetectionNode::ArucoDetectionNode() 
+: loop_rate(10) 
+, dictionary(cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250))
+, detectorParameters(cv::aruco::DetectorParameters::create())
+, markerLength(0.05)
+, cameraMatrix(cv::Mat::eye(3, 3, CV_32F))
+, distCoeffs(cv::Mat::zeros(3, 1, CV_32F))
+{
+    op_sub = node.subscribe("/udfc/wrapper/camera_rect",10, &ArucoDetectionNode::callback, this);
     op_pub = node.advertise<aruco_msgs::MarkerArray>("arUco_marker_positions_out",10);
-
-    dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
-    parameters = cv::aruco::DetectorParameters::create();
 }
 
 void ArucoDetectionNode::callback(const sensor_msgs::ImageConstPtr& img_source){
@@ -19,32 +18,95 @@ void ArucoDetectionNode::callback(const sensor_msgs::ImageConstPtr& img_source){
     // Convert ROS-image to CV-image
     ////////////////////////////////
 
-    const cv_bridge::CvImageConstPtr cvImage = cv_bridge::toCvShare(img_source,"");
-
+    const cv_bridge::CvImageConstPtr cvImage = cv_bridge::toCvShare(img_source, "");
 
     // Detect markers //////////////
     ////////////////////////////////
 
-    cv::Mat inputImage;
-
     std::vector<int> markerIds;
     std::vector<std::vector<cv::Point2f>> markerCorners, rejectedCandidates;
+    cv::aruco::detectMarkers(cvImage->image, dictionary, markerCorners, markerIds, detectorParameters, rejectedCandidates);
 
-    cv::aruco::detectMarkers(inputImage, dictionary, markerCorners, markerIds, parameters, rejectedCandidates);
+    // Estimate poses //////////////
+    ////////////////////////////////
+    aruco_msgs::MarkerArray markerMsg;
+    estimateMarkerPoses(markerIds, markerCorners, markerMsg);
 
-
-    // op_pub.publish(objPos);            
+    op_pub.publish(markerMsg);            
 }
 
 void ArucoDetectionNode::execute(){
-    while (ros::ok()){
+    while (ros::ok()) {
         ros::spinOnce();
         loop_rate.sleep();
     }
 }
 
+void ArucoDetectionNode::estimateMarkerPoses(
+    std::vector<int> markerIds, 
+    std::vector<std::vector<cv::Point2f>> markerCorners, 
+    aruco_msgs::MarkerArray& markerMsg) {
+
+    // Estimate poses //////////////
+    ////////////////////////////////
+    std::vector<cv::Vec3d> rvecs, tvecs;
+    cv::aruco::estimatePoseSingleMarkers(markerCorners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+
+
+    // Put result in a ROS message /
+    ////////////////////////////////    
+
+    std_msgs::Header msgHeader;
+    static size_t seq_id{0};
+    
+    msgHeader.frame_id = "UDFC_frame";
+    msgHeader.seq      = seq_id++;
+    msgHeader.stamp    = ros::Time::now();
+
+    markerMsg.header = msgHeader;
+
+    for (size_t i; i<markerIds.size(); i++) {
+        aruco_msgs::Marker marker;
+        marker.header                = msgHeader;
+        marker.id                    = markerIds.at(i);
+        marker.pose.pose.orientation = rvecs.at(i);
+        marker.pose.pose.position    = tvecs.at(i);
+        markerMsg.markers.push_back(marker);
+    }
+}
+
+void ArucoDetectionNode::estimateBoardPose(
+    std::vector<int> markerIds, 
+    std::vector<std::vector<cv::Point2f>> markerCorners, 
+    geometry_msgs::Pose boardPose) {
+
+    // if at least one marker is detected
+    cv::Vec3d rvec, tvec;
+    int valid{0};
+    if (markerIds.size() > 0) {
+        valid = cv::aruco::estimatePoseBoard(markerCorners, markerIds, &board, cameraMatrix, distCoeffs, rvec, tvec);
+    }
+    if (!valid) return;
+    double angle = cv::norm(rvec);
+    double nu    = 
+
+    boardPose.orientation = geometry_msgs::Point(tvec);
+    boardPose.position    = tvec;
+}
+
+
+
+
+
 
 int main(int argc, char **argv){
+    // Needs config:
+    //      Camera calibration
+    //      Marker dictionary
+    //      Marker length
+    //      Board
+    //      Frame
+    //      Camera subscribe topic
     ros::init(argc,argv,"ArucoDetectionNode");
     ArucoDetectionNode arucoNode;
     arucoNode.execute();
