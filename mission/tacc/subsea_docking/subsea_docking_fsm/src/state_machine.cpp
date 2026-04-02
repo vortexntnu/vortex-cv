d47450bed385bece855aa873b77bd26b3dd8b210
CurryMonsters <hinthu5@hotmail.no>
2026-04-02 22:11:23 +0200

    fallback_waypoint is now concurrence with landmark_polling

--- mission/tacc/subsea_docking/subsea_docking_fsm/src/state_machine.cpp
+++ mission/tacc/subsea_docking/subsea_docking_fsm/src/state_machine.cpp
@@ -1,5 +1,7 @@
 #include "subsea_docking_fsm/states.hpp"
 
+#include <unordered_set>
+
 #include <yasmin/cb_state.hpp>
 #include <yasmin_ros/basic_outcomes.hpp>
 #include <yasmin_ros/ros_logs.hpp>
@@ -80,21 +82,32 @@ std::shared_ptr<yasmin::StateMachine> build_state_machine(
 
         sm->add_state("SEARCH", search,
                       {{"landmark_found", "LANDMARK_CONVERGENCE"},
-                       {"service_timeout", "FALLBACK_WAYPOINT"},
+                       {"service_timeout", "FALLBACK_SEARCH"},
                        {ABORT, ABORT},
                        {CANCEL, ABORT}});
 
-        sm->add_state("FALLBACK_WAYPOINT",
-                      std::make_shared<WaypointGoalState>(
-                          config.waypoint_manager_action_server, fallback_goal),
-                      {{SUCCEED, "FALLBACK_LANDMARK_POLLING"}, {ABORT, ABORT}});
+        auto fallback_waypoint = std::make_shared<WaypointGoalState>(
+            config.waypoint_manager_action_server, fallback_goal);
 
-        sm->add_state(
-            "FALLBACK_LANDMARK_POLLING",
-            std::make_shared<LandmarkPollingState>(
-                config.landmark_polling_action_server, landmark_type,
-                landmark_subtype, "landmarks"),
-            {{"landmarks_found", "LANDMARK_CONVERGENCE"}, {ABORT, ABORT}});
+        auto fallback_landmark_polling = std::make_shared<LandmarkPollingState>(
+            config.landmark_polling_action_server, landmark_type,
+            landmark_subtype, "landmarks");
+
+        auto fallback_search = std::make_shared<FirstWinsConcurrence>(
+            yasmin::StateMap{
+                {"FALLBACK_WAYPOINT", fallback_waypoint},
+                {"FALLBACK_LANDMARK_POLLING", fallback_landmark_polling}},
+            ABORT,
+            FirstWinsOutcomeMap{
+                {"FALLBACK_WAYPOINT", {{SUCCEED, ABORT}, {ABORT, ABORT}}},
+                {"FALLBACK_LANDMARK_POLLING",
+                 {{"landmarks_found", "landmark_found"}, {ABORT, ABORT}}}},
+            std::unordered_set<std::string>{"FALLBACK_LANDMARK_POLLING"});
+
+        sm->add_state("FALLBACK_SEARCH", fallback_search,
+                      {{"landmark_found", "LANDMARK_CONVERGENCE"},
+                       {ABORT, ABORT},
+                       {CANCEL, ABORT}});
     }
 
     sm->add_state("LANDMARK_CONVERGENCE",
