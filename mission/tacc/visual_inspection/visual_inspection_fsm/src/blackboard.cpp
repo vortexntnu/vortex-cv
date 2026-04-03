33ab4041d45b0be303ca9447af346a6bcf64eccc
CurryMonsters <hinthu5@hotmail.no>
2026-04-03 14:48:17 +0200

    moved frames

--- mission/tacc/visual_inspection/visual_inspection_fsm/src/blackboard.cpp
+++ mission/tacc/visual_inspection/visual_inspection_fsm/src/blackboard.cpp
@@ -11,12 +11,18 @@ std::shared_ptr<yasmin::Blackboard> initialize_blackboard() {
         "action_servers.landmark_polling");
     const std::string start_service =
         params->declare_parameter<std::string>("services.start_mission");
+    const std::string gripper_frame =
+        params->declare_parameter<std::string>("frames.gripper_frame");
+    const std::string base_frame =
+        params->declare_parameter<std::string>("frames.base_frame");
 
     auto bb = std::make_shared<yasmin::Blackboard>();
 
     bb->set<std::string>("action_server.landmark_convergence", lc_action);
     bb->set<std::string>("action_server.landmark_polling", lp_action);
     bb->set<std::string>("service.start_mission", start_service);
+    bb->set<std::string>("frames.gripper_frame", gripper_frame);
+    bb->set<std::string>("frames.base_frame", base_frame);
 
     return bb;
 }
