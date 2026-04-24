fdcea70c121b9c68e7bcbc63bfb89aedabc92d19
Ashish <ashishbhardwaj2005@gmail.com>
2026-04-24 15:42:04 +0200

    Small fix in naming of parameter

--- mission/tacc/pipeline_inspection/pipeline_end_detector/include/pipeline_end_detector/pipeline_end_detector_node.hpp
+++ mission/tacc/pipeline_inspection/pipeline_end_detector/include/pipeline_end_detector/pipeline_end_detector_node.hpp
@@ -20,9 +20,9 @@ class PipelineEndDetectorNode : public rclcpp::Node {
     rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr end_of_pipeline_client_;
 
     int consecutive_detections_{
-        0};                   // number of consecutive Class 1 detections so far
-    int detection_threshold;  // number of consecutive detections required to
-                              // trigger the service call
+        0};  // number of consecutive Class 1 detections so far
+    int detection_threshold_;  // number of consecutive detections required to
+                               // trigger the service call
     bool service_called_{
         false};  // prevents the service from being called more than once
 };
