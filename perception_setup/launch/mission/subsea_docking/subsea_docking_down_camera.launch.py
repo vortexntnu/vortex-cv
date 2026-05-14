22d3c2579a4361bcb912ea5eb0670dc48669b87b
vortexuser <vortex.git@vortexntnu.no>
2026-05-14 18:00:42 +0000

    use updated gst name

--- perception_setup/launch/mission/subsea_docking/subsea_docking_down_camera.launch.py
+++ perception_setup/launch/mission/subsea_docking/subsea_docking_down_camera.launch.py
@@ -84,9 +84,9 @@ def _launch_setup(context, *args, **kwargs):
     if enable_gstreamer:
         container_nodes.append(
             ComposableNode(
-                package='image_to_gstreamer',
-                plugin='image_to_gstreamer::ImageToGStreamer',
-                name='image_to_gstreamer_node',
+                package='gstreamer_from_ros',
+                plugin='gstreamer_from_ros::GStreamerFromRos',
+                name='gstreamer_from_ros_node',
                 parameters=[{
                     'input_topic': _ARUCO_IMAGE_TOPIC,
                     'destination_ip': destination_ip,
